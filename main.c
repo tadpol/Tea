26df10d99a6ce3275d43705f3d46ab307a5d2b08
Michael Conrad Tadpol Tilstra <miketilstra@exosite.com>
2014-07-25 09:43:15 -0500

    Reordered and print progress of tests.

--- main.c
+++ main.c
@@ -31,19 +31,16 @@ int main(int argc, char **argv)
     assert(isinf(tea_calc("infinity", NULL)));
     assert(isinf(tea_calc("inf", NULL)));
     assert(isinf(tea_calc("INF", NULL)));
+    puts(" Passed basic number parsing");
 
     assert(11.0f == tea_calc("6+5", NULL));
     assert(11.0f == tea_calc(" 6 + 5 ", NULL));
-
     assert(5.0f == tea_calc("14 - 9", NULL));
-
     assert(12.0f == tea_calc("24/2", NULL));
-
     assert(36.0f == tea_calc("9*4", NULL));
-
     assert(4.0f == tea_calc("14%10", NULL));
-
     assert(81.0f == tea_calc("9^2", NULL));
+    puts(" Passed basic operators");
 
     assert(1.0f == tea_calc("10 > 5", NULL));
     assert(0.0f == tea_calc("5 > 10", NULL));
@@ -62,6 +59,7 @@ int main(int argc, char **argv)
     assert(1.0f == tea_calc("10 == 10", NULL));
     assert(1.0f == tea_calc("10 != 5", NULL));
     assert(0.0f == tea_calc("5 != 5", NULL));
+    puts(" Passed Comparisions");
 
     assert(1.0f == tea_calc("1 && 1", NULL));
     assert(0.0f == tea_calc("1 && 0", NULL));
@@ -82,6 +80,7 @@ int main(int argc, char **argv)
     assert(1.0f == tea_calc("2 || 0", NULL));
     assert(1.0f == tea_calc("0 || 8", NULL));
     assert(0.0f == tea_calc("0 || 0", NULL));
+    puts(" Passed && and || tests");
 
     // TODO: Add tests for assignment
 
@@ -89,26 +88,29 @@ int main(int argc, char **argv)
     assert(27.0f == tea_calc("45 - 6 * 3", NULL));
     assert(27.0f == tea_calc("45 - (6 * 3)", NULL));
     assert(117.0f == tea_calc("(45 - 6) * 3", NULL));
-
     assert(isnan(tea_calc("5 + ( 6", NULL)));
-    assert(isnan(tea_calc("bobble(6)", NULL)));
-    assert(isnan(tea_calc("bobble(6, 9)", NULL)));
-    assert(isnan(tea_calc("tan(6", NULL)));
-    assert(isnan(tea_calc("pow(6", NULL)));
-    assert(isnan(tea_calc("pow(6,", NULL)));
-    assert(isnan(tea_calc("pow(6,8", NULL)));
-
+    puts(" Passed parenthese tests");
 
     constants[0] = 200.0f;
     constants[1] = 55.0f;
     assert(200.0f == tea_calc("A", constants));
     assert(255.0f == tea_calc("A + B", constants));
     assert(isnan(tea_calc("Z", NULL)));
+    puts(" Passed preloaded variable tests");
+
+    assert(isnan(tea_calc("bobble(6)", NULL)));
+    assert(isnan(tea_calc("bobble(6, 9)", NULL)));
+    assert(isnan(tea_calc("tan(6", NULL)));
+    assert(isnan(tea_calc("pow(6", NULL)));
+    assert(isnan(tea_calc("pow(6,", NULL)));
+    assert(isnan(tea_calc("pow(6,8", NULL)));
+    puts(" Passed malformed function call tests");
 
     assert(42.0 == tea_calc("abs(-42)", NULL));
     assert(42.0 == tea_calc(" abs( -42 ) ", NULL));
     assert(81.0f == tea_calc("pow(9,2)", NULL));
     assert(81.0f == tea_calc(" pow ( 9 , 2 ) ", NULL));
+    puts(" Passed function call tests");
 
     constants[0] = 4234.0f;
     constants[1] = 4050.0f;
